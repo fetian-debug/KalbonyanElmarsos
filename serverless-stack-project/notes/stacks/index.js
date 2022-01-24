cb98685d80ff6f87e679cde43ede9a7a0fa8113a
fetian ans <fetianans@gmail.com>
2022-01-25 01:58:09 +0200

    Securing the API

--- serverless-stack-project/notes/stacks/index.js
+++ serverless-stack-project/notes/stacks/index.js
@@ -1,10 +1,17 @@
 import StorageStack from "./StorageStack";
 import ApiStack from "./ApiStack";
+import AuthStack from "./AuthStack";
+
 
 export default function main(app) {
   const storageStack = new StorageStack(app, "storage");
 
-  new ApiStack(app, "api", {
+  const apiStack = new ApiStack(app, "api", {
     table: storageStack.table,
+  });
+
+  new AuthStack(app, "auth", {
+    api: apiStack.api,
+    bucket: storageStack.bucket,
   });
 }
