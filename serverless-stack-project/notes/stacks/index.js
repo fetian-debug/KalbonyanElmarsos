6029208a1a20c010c7a01a1b91b711b263cc533d
fetian ans <fetianans@gmail.com>
2022-01-24 23:33:22 +0200

    Adding a storage stack

--- serverless-stack-project/notes/stacks/index.js
+++ serverless-stack-project/notes/stacks/index.js
@@ -1,12 +1,5 @@
-import MyStack from "./MyStack";
+import StorageStack from "./StorageStack";
 
 export default function main(app) {
-  // Set default runtime for all functions
-  app.setDefaultFunctionProps({
-    runtime: "nodejs14.x"
-  });
-
-  new MyStack(app, "my-stack");
-
-  // Add more stacks
+  new StorageStack(app, "storage");
 }
