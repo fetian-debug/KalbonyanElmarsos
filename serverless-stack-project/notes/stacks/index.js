c08b4d70726761ecdedec743e77d53a806e9af0e
fetian ans <fetianans@gmail.com>
2022-01-25 01:31:51 +0200

    Adding the API

--- serverless-stack-project/notes/stacks/index.js
+++ serverless-stack-project/notes/stacks/index.js
@@ -1,5 +1,10 @@
 import StorageStack from "./StorageStack";
+import ApiStack from "./ApiStack";
 
 export default function main(app) {
-  new StorageStack(app, "storage");
+  const storageStack = new StorageStack(app, "storage");
+
+  new ApiStack(app, "api", {
+    table: storageStack.table,
+  });
 }
