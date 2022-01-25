74fd4830a7b5f05239d1b474fb3a7dcb81481833
fetian ans <fetianans@gmail.com>
2022-01-25 03:09:32 +0200

    Enabling CORS

--- serverless-stack-project/notes/src/util/handler.js
+++ serverless-stack-project/notes/src/util/handler.js
@@ -14,8 +14,12 @@ export default function handler(lambda) {
 
     // Return HTTP response
     return {
-      statusCode,
-      body: JSON.stringify(body),
-    };
+  statusCode,
+  body: JSON.stringify(body),
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+  },
+  };
   };
 }
