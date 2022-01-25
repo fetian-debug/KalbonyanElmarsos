cd3b47dc54e27a3948f9af9140db43ea1c836fdf
fetian ans <fetianans@gmail.com>
2022-01-25 03:06:07 +0200

    Adding unit tests

--- serverless-stack-project/notes/src/billing.js
+++ serverless-stack-project/notes/src/billing.js
@@ -21,16 +21,3 @@ export const main = handler(async (event) => {
 });
 
 
-
-// npx aws-api-gateway-cli-test \
-// --username='admin@example.com' \
-// --password='Passw0rd!' \
-// --user-pool-id='us-east-1_0XHbnHSdC' \
-// --app-client-id='11m9np8nlg38pbt5jb0ovb0iv6' \
-// --cognito-region='us-east-1' \
-// --identity-pool-id='us-east-1:74130c19-6648-4b39-8210-f600da491ce8' \
-// --invoke-url='https://j3guoc4ro9.execute-api.us-east-1.amazonaws.com' \
-// --api-gateway-region='us-east-1' \
-// --path-template='/billing' \
-// --method='POST' \
-// --body='{"source":"tok_visa","storage":21}'
