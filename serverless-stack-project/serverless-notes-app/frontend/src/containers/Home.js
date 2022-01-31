a9cf7c0ef07e8d83fe4162c801d364f8bc876630
fetian ans <fetianans@gmail.com>
2022-01-31 23:29:09 +0200

    Adding invalid method

--- serverless-stack-project/serverless-notes-app/frontend/src/containers/Home.js
+++ serverless-stack-project/serverless-notes-app/frontend/src/containers/Home.js
@@ -32,7 +32,7 @@ export default function Home() {
   }, [isAuthenticated]);
 
   function loadNotes() {
-    return API.get("notes", "/invalid_path");
+    return API.get("notes", "/notes");
   }
 
   function renderNotesList(notes) {
