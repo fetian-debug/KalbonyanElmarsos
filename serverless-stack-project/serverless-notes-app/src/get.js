bec9a2f417af8e2695722bff5598b595aa96351a
fetian ans <fetianans@gmail.com>
2022-01-31 23:03:36 +0200

    Adding some faulty code

--- serverless-stack-project/serverless-notes-app/src/get.js
+++ serverless-stack-project/serverless-notes-app/src/get.js
@@ -5,6 +5,9 @@ export const main = handler(async (event) => {
   const params = {
     TableName: process.env.TABLE_NAME,
     // 'Key' defines the partition key and sort key of the item to be retrieved
+   // - 'userId'
+  // - 'note ID'
+
     Key: {
       userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, // The id of the author
       noteId: event.pathParameters.id, // The id of the note from the path
